8bc76e68a580c2f4d739656efb699589f75e5265
usertiagosilva <tiago_ferreira.s@hotmail.com>
2024-06-07 20:47:58 -0300

    commit v2  menu toggle

--- babershop/script.js
+++ babershop/script.js
@@ -1,1 +1,7 @@
 AOS.init();
+
+// MENU TOGGLE
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('active');
+  });
+  
